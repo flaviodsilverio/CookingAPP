33151f640eb86e43c77ea0f7c076711573fe79c4
Flávio Silvério <flaviosilverio@MBP-de-Flavio.lan>
2015-03-10 18:33:21 +0000

    Initial Commit

---
+++ CookingAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CookingAPP
+//
+//  Created by Flávio Silvério on 10/03/15.
+//  Copyright (c) 2015 Flávio Silvério. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CookingAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CookingAPP
+//
+//  Created by Flávio Silvério on 10/03/15.
+//  Copyright (c) 2015 Flávio Silvério. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
